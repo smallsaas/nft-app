5b3c6867ff188b1566fbf4498a06b886681ee4ee
AEdgejay <471021444@qq.com>
2021-12-16 16:55:43 +0800

    解决开发环境跨域问题

--- config.js
+++ config.js
@@ -3,7 +3,14 @@ import {cache} from '@/utils/tools.js'
 
 const globalConfigStorage = cache.get('globalConfigStorage')
 
-export const globalConfig = {
+// 是否是开发环境
+// 开发环境中的api需要在manifest.json中的源码视图>h5>devServer>proxy中添加路径即可
+const isDev = true
+export const globalConfig = isDev?{
+	formHost: "/form",
+	dataHost: "/data",
+	endpoint: "/"
+}:{
 	formHost: _.get(globalConfigStorage,"formHost","https://api.mock.smallsaas.cn/form"),
   dataHost: _.get(globalConfigStorage, 'dataHost', 'https://api.mock.smallsaas.cn/data'),
   endpoint: _.get(globalConfigStorage, 'endpoint', 'https://api.mock.smallsaas.cn'),
