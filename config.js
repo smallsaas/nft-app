d094ed6186c5393e7929c4be3e90de1cad1110ce
AEdgejay <471021444@qq.com>
2021-12-28 19:57:25 +0800

    update skeleton_row css

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ const iconList = cache.get("iconList")
 
 // 是否是开发环境
 // 开发环境中的api需要在manifest.json中的源码视图>h5>devServer>proxy中添加路径即可
-const isDev = false
+const isDev = true
 export const globalConfig = isDev?{
 	formHost: "http://localhost:8080/form",
 	dataHost: "http://localhost:8080/data",
