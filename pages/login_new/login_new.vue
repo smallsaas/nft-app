7f1edfc4abf4ed01092d5fa9b11b9c740c86c7d3
HDJ <263463764@qq.com>
2022-01-26 11:56:07 +0800

    注释测试账号

--- pages/login_new/login_new.vue
+++ pages/login_new/login_new.vue
@@ -37,8 +37,8 @@
 				isOpenLook:['../../static/login/eyeoff.png','../../static/login/eye.png'],
 				openIndex:0,
 				data:{
-					account:'13425836900',
-					password:'a123456',
+					// account:'13425836900',
+					// password:'a123456',
 					// account:'15322315902',
 					// password:'admin1234',
 					//18689911298
