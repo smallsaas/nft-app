e3955efa8f5a822950539fc7d1438d8c65e09ad2
傅庆发 <979338111@qq.com>
2022-03-14 16:15:14 +0800

    修正ios时间乱码

--- components/dynamic-list/listItem/myAdoptSpirit_new.vue
+++ components/dynamic-list/listItem/myAdoptSpirit_new.vue
@@ -1,304 +1,303 @@
 <template>
-	<view class="box">
-		<view class="item">
-			<view class="imgBox">
-				<image :src="getImage(item.wisp.previewPhotoUrl)" mode="widthFix" class="img"></image>
-			</view>
-			<view class="nameBox">
-				<text class="name">{{item.wisp.name}}</text>
-			</view>
-			<view class="levelBox">
-				<image class="levelImg" src="../../../static/level/one.png" mode="widthFix"
-					v-if="item.wisp.level==1"></image>
-				<image class="levelImg" src="../../../static/level/two.png" mode="widthFix"
-					v-if="item.wisp.level==2"></image>
-				<image class="levelImg" src="../../../static/level/three.png" mode="widthFix"
-					v-if="item.wisp.level==3"></image>
-				<image class="levelImg" src="../../../static/level/four.png" mode="widthFix"
-					v-if="item.wisp.level==4"></image>
-				<image class="levelImg" src="../../../static/level/five.png" mode="widthFix"
-					v-if="item.wisp.level==5"></image>
-				<image class="levelImg" src="../../../static/level/six.png" mode="widthFix"
-					v-if="item.wisp.level==6"></image>
-				<image class="levelImg" src="../../../static/level/seven.png" mode="widthFix"
-					v-if="item.wisp.level==7"></image>
-				<image class="levelImg" src="../../../static/level/eight.png" mode="widthFix"
-					v-if="item.wisp.level==8"></image>
-			</view>
-            <view class="infoBox wispNumber">
-            	<text class="info">編號：{{item.wispNumber}}</text>
-            </view>
-			<!-- <view class="infoBox infoBoxFive">
+  <view class="box">
+    <view class="item">
+      <view class="imgBox">
+        <image :src="getImage(item.wisp.previewPhotoUrl)" mode="widthFix" class="img"></image>
+      </view>
+      <view class="nameBox">
+        <text class="name">{{ item.wisp.name }}</text>
+      </view>
+      <view class="levelBox">
+        <image class="levelImg" src="../../../static/level/one.png" mode="widthFix" v-if="item.wisp.level == 1"></image>
+        <image class="levelImg" src="../../../static/level/two.png" mode="widthFix" v-if="item.wisp.level == 2"></image>
+        <image class="levelImg" src="../../../static/level/three.png" mode="widthFix" v-if="item.wisp.level == 3"></image>
+        <image class="levelImg" src="../../../static/level/four.png" mode="widthFix" v-if="item.wisp.level == 4"></image>
+        <image class="levelImg" src="../../../static/level/five.png" mode="widthFix" v-if="item.wisp.level == 5"></image>
+        <image class="levelImg" src="../../../static/level/six.png" mode="widthFix" v-if="item.wisp.level == 6"></image>
+        <image class="levelImg" src="../../../static/level/seven.png" mode="widthFix" v-if="item.wisp.level == 7"></image>
+        <image class="levelImg" src="../../../static/level/eight.png" mode="widthFix" v-if="item.wisp.level == 8"></image>
+      </view>
+      <view class="infoBox wispNumber">
+        <text class="info">編號：{{ item.wispNumber }}</text>
+      </view>
+      <!-- <view class="infoBox infoBoxFive">
 				<text class="info">能力值：{{item.coinsPrice}}/{{item.realtimePrice}}</text>
 				<text class="info">能力值：{{item.amountPrice}}/{{parseInt(item.amountPrice) + item.coinsAddedValue}}</text>
 			</view> -->
-			<view class="infoBox infoBoxFive">
-				<text class="info">領養能力值：{{item.coinsPrice}}</text>
-			</view>
-			<view class="infoBox infoBoxSix">
-				<text class="info">當前能力值：{{item.realtimePrice}}</text>
-			</view>
-			<view class="infoBox infoBoxSeven" style="display: flex; flex-direction: row; align-items: center;">
-				<text class="info">産生收益：</text><view style="color: #FF0000;font-size: 13px;">{{(item.realtimePrice-item.coinsPrice).toFixed(2)}}</view>
-			</view> 
-			<view class="infoBox infoBoxTwo">
-				<text class="info">增長能力：{{item.wisp.growthPercent}}%</text>
-			</view>
-			<view class="infoBox infoBoxThree">
-				<text class="info">培養天數：{{item.wisp.growthDays}}</text>
-			</view>
-			<view class="infoBox infoBoxFour">
-				<text class="info sss">領養：{{item.stageChangeTime}}</text>
-			</view>
-			<view class="line"></view>
-			<view class="btnBox">
-				<button class="btn one" v-if="true" @click="noMessage">鑄造(待開放)</button>
-				<view class="boxs" v-if="true">
-					<button class="two">(培養中)自動挂售</button>
-				</view>
-				<!-- <text class="times" v-if="true">成長中</text>
+      <view class="infoBox infoBoxFive">
+        <text class="info">領養能力值：{{ item.coinsPrice }}</text>
+      </view>
+      <view class="infoBox infoBoxSix">
+        <text class="info">當前能力值：{{ item.roundHalfUpRealtimePrice }}</text>
+      </view>
+      <view class="infoBox infoBoxSeven" style="display: flex; flex-direction: row; align-items: center">
+        <text class="info">産生收益：</text><view style="color: #ff0000; font-size: 13px">{{ realtimeProfit.toFixed(2) }}</view>
+      </view>
+      <view class="infoBox infoBoxTwo">
+        <text class="info">增長能力：{{ item.wisp.growthPercent }}%</text>
+      </view>
+      <view class="infoBox infoBoxThree">
+        <text class="info">培養天數：{{ item.wisp.growthDays }}</text>
+      </view>
+      <view class="infoBox infoBoxFour">
+        <text class="info sss">領養：{{ item.stageChangeTime }}</text>
+      </view>
+      <view class="line"></view>
+      <view class="btnBox">
+        <button class="btn one" v-if="true" @click="noMessage">鑄造(待開放)</button>
+        <view class="boxs" v-if="true">
+          <button class="two">(培養中)自動挂售</button>
+        </view>
+        <!-- <text class="times" v-if="true">成長中</text>
 				<text class="time" v-if="true">剩餘時間 48:52:47</text> -->
-			</view>
-		</view>
-		<toast v-if="isShowToast" :data="toastMsg" @cancelToast="closeToast"></toast>
-	</view>
+      </view>
+    </view>
+    <toast v-if="isShowToast" :data="toastMsg" @cancelToast="closeToast"></toast>
+  </view>
 </template>
 
 <script>
-	import toast from '../../spirit/toast.vue'
-	export default{
-		components:{toast},
-		props: {
-			item: Object,
-			ext: Object
-		},
-		created() {
-			// const t = this.item.stageChangeTime.split('T')
-			// console.log(t,'--------------')
-			// this.timeString = t[0] + ' ' + t[1]
-		},
-		data() {
-			return {
-				isShowToast:false,
-				toastMsg:'',
-				timeString:'',
-			}
-		},
-		methods:{
-			getImage(url) {
-				// console.log(this.$config)
-				let that = this
-				let imagePath;
-				if (url.indexOf("http" || "https") === 0) {
-					imagePath = url
-				} else {
-					// console.log(url.indexOf("["))
-					if (url.indexOf("[") === 0) {
-						let urlJSON = JSON.parse(url)
-						let imageUrl = urlJSON[1].url
-						if (imageUrl.indexOf("http" || "https") === 0) {
-							imagePath = imageUrl
-						} else {
-							if(![undefined,null,''].includes(that.$config.endpoint)){
-								imagePath = that.$config.endpoint + "/" +imageUrl;								
-							}else{
-								imagePath = that.$config.imageEndpoint + "/" +imageUrl
-							}
-							// return this.$config.endpoint + "/" + imageUrl
-						}
-					}
-				}
-				return imagePath
-			},
-			toast(msg) {
-				this.toastMsg = msg
-				this.isShowToast = true
-			},
-			noMessage(){
-				this.toast('功能暫未開放，敬請期待!')
-			},
-			closeToast(){
-				this.isShowToast = false
-			}
-		}
-	}
+import toast from '../../spirit/toast.vue'
+export default {
+  components: { toast },
+  props: {
+    item: Object,
+    ext: Object
+  },
+  created () {
+    // const t = this.item.stageChangeTime.split('T')
+    // console.log(t,'--------------')
+    // this.timeString = t[0] + ' ' + t[1]
+  },
+  data () {
+    return {
+      isShowToast: false,
+      toastMsg: '',
+      timeString: '',
+    }
+  },
+  methods: {
+    getImage (url) {
+      // console.log(this.$config)
+      let that = this
+      let imagePath;
+      if (url.indexOf("http" || "https") === 0) {
+        imagePath = url
+      } else {
+        // console.log(url.indexOf("["))
+        if (url.indexOf("[") === 0) {
+          let urlJSON = JSON.parse(url)
+          let imageUrl = urlJSON[1].url
+          if (imageUrl.indexOf("http" || "https") === 0) {
+            imagePath = imageUrl
+          } else {
+            if (![undefined, null, ''].includes(that.$config.endpoint)) {
+              imagePath = that.$config.endpoint + "/" + imageUrl;
+            } else {
+              imagePath = that.$config.imageEndpoint + "/" + imageUrl
+            }
+            // return this.$config.endpoint + "/" + imageUrl
+          }
+        }
+      }
+      return imagePath
+    },
+    toast (msg) {
+      this.toastMsg = msg
+      this.isShowToast = true
+    },
+    noMessage () {
+      this.toast('功能暫未開放，敬請期待!')
+    },
+    closeToast () {
+      this.isShowToast = false
+    }
+  }
+}
 </script>
 
 <style lang="less">
-	.box{
-		width: 100%;
-		height: 100%;
-		background: #050E17;
-		padding-top: 20rpx;
-		// padding-bottom: 20rpx;
-		.item{
-			margin: 0rpx auto;
-			width: 686rpx;
-			height: 598rpx;
-			background: linear-gradient(135deg, #1D294F 0%, #17253F 100%);
-			border-radius: 8px 8px 8px 8px;
-			opacity: 1;
-			border-top: 2rpx solid rgb(50, 71, 137);
-			border-left: 2rpx solid rgb(50, 71, 137);
-			position: relative;
-			.imgBox{
-				width: 280rpx;
-				height: 280rpx;
-				border-radius: 0px 0px 0px 0px;
-				opacity: 1;
-				position: absolute;
-				top: 24rpx;
-				left: 24rpx;
-				.img{
-					width: 280rpx;
-					height: 280rpx !important;
-				}
-			}
-			.nameBox{
-				width: 350rpx !important;
-				height: 44rpx;
-				position: absolute;
-				top: 32rpx;
-				left: 328rpx;
-				.name{
-					font-size: 16px;
-					font-family: PingFang SC-Bold, PingFang SC;
-					font-weight: bold;
-					color: #FFFFFF;
-				}
-			}
-			.levelBox{
-				width: 134rpx;
-				height: 36rpx !important;
-				position: absolute;
-				top: 92rpx;
-				left: 328rpx;
-				.levelImg{
-					width: 134rpx;
-					height: 36rpx !important;
-				}
-			}
-				
-			.infoBox,.infoBoxFive,.infoBoxTwo,.infoBoxThree, .wispNumber, .infoBoxFour, .infoBoxSix, .infoBoxSeven{
-				width: 350rpx !important;
-				// height: 34rpx;
-				position: absolute;
-                line-height: 1;
-				top: 148rpx;
-				left: 328rpx;
-				.info{
-					font-size: 12px;
-					font-family: PingFang SC-Regular, PingFang SC;
-					font-weight: 400;
-					color: #FFFFFF;
-				}
-				.sss{
-					top: 168rpx !important;
-				}
-			}
-            .wispNumber {
-                top: 148rpx;
-            }
-            
-            .infoBoxFive {
-                top: 192rpx;
-            }
-			
-			.infoBoxSix{
-                top: 236rpx;
-			}
-			
-			.infoBoxSeven{
-				top: 286rpx;
-			}
-			
-			.infoBoxTwo{
-				top: 324rpx;
-			}
-			.infoBoxThree{
-                top: 368rpx;
-				.sss{
-					position: relative;
-					top: 40rpx !important;
-				}
-			}
-            .infoBoxFour {
-                top: 412rpx;
-            }
+.box {
+  width: 100%;
+  height: 100%;
+  background: #050e17;
+  padding-top: 20rpx;
+  // padding-bottom: 20rpx;
+  .item {
+    margin: 0rpx auto;
+    width: 686rpx;
+    height: 598rpx;
+    background: linear-gradient(135deg, #1d294f 0%, #17253f 100%);
+    border-radius: 8px 8px 8px 8px;
+    opacity: 1;
+    border-top: 2rpx solid rgb(50, 71, 137);
+    border-left: 2rpx solid rgb(50, 71, 137);
+    position: relative;
+    .imgBox {
+      width: 280rpx;
+      height: 280rpx;
+      border-radius: 0px 0px 0px 0px;
+      opacity: 1;
+      position: absolute;
+      top: 24rpx;
+      left: 24rpx;
+      .img {
+        width: 280rpx;
+        height: 280rpx !important;
+      }
+    }
+    .nameBox {
+      width: 350rpx !important;
+      height: 44rpx;
+      position: absolute;
+      top: 32rpx;
+      left: 328rpx;
+      .name {
+        font-size: 16px;
+        font-family: PingFang SC-Bold, PingFang SC;
+        font-weight: bold;
+        color: #ffffff;
+      }
+    }
+    .levelBox {
+      width: 134rpx;
+      height: 36rpx !important;
+      position: absolute;
+      top: 92rpx;
+      left: 328rpx;
+      .levelImg {
+        width: 134rpx;
+        height: 36rpx !important;
+      }
+    }
 
-			.line{
-				width: 684rpx;
-				height: 2rpx;
-				background: linear-gradient(270deg, #182641 0%, #3F547D 49%, #182641 100%);
-				border-radius: 0px 0px 0px 0px;
-				opacity: 1;
-				position: absolute;
-				top: 462rpx;
-			}
-			.btnBox{
-				width: 100%;
-				height: 142rpx;
-				position: absolute;
-				bottom: 0rpx;
-				display: flex;
-				align-items: center;
-				justify-content: space-around;
-				.times{
-					position: absolute;
-					left: 32rpx;
-					width: 119px;
-					height: 22px;
-					font-size: 14px;
-					font-family: PingFang SC-Regular, PingFang SC;
-					font-weight: 400;
-					color: #31E4B9;
-				}
-				.time{
-					position: absolute;
-					right: 32rpx;
-					width: 119px;
-					height: 22px;
-					font-size: 14px;
-					font-family: PingFang SC-Regular, PingFang SC;
-					font-weight: 400;
-					color: #FFFFFF;
-				}
-				.btn{
-					width: 300rpx;
-					height: 80rpx;
-					background: linear-gradient(270deg, #9331F5 0%, #0B95FF 100%);
-					border-radius: 8px 8px 8px 8px;
-					opacity: 1;
-					font-size: 16px;
-					font-family: PingFang SC-Medium, PingFang SC;
-					font-weight: 500;
-					color: #FFFFFF;
-					display: flex;
-					align-items: center;
-					justify-content: center;
-				}
-				.boxs{
-					width: 300rpx !important;
-					height: 80rpx !important;
-					padding: 2rpx;
-					border-radius: 8px;
-					border: 1px solid;
-					background-image: linear-gradient(to right bottom,rgba(147, 49, 245, 1),rgba(11, 149, 255, 1));
-					margin-right: 32rpx;
-					.two{
-						width: 300rpx !important;
-						height: 80rpx !important;
-						font-size: 15px;
-						font-family: PingFang SC-Medium, PingFang SC;
-						font-weight: 500;
-						color: #FFFFFF;
-						display: flex;
-						align-items: center;
-						justify-content: center;
-						background: rgb(25,38,68);
-					}
-				}
-			}
-		}
-	}
+    .infoBox,
+    .infoBoxFive,
+    .infoBoxTwo,
+    .infoBoxThree,
+    .wispNumber,
+    .infoBoxFour,
+    .infoBoxSix,
+    .infoBoxSeven {
+      width: 350rpx !important;
+      // height: 34rpx;
+      position: absolute;
+      line-height: 1;
+      top: 148rpx;
+      left: 328rpx;
+      .info {
+        font-size: 12px;
+        font-family: PingFang SC-Regular, PingFang SC;
+        font-weight: 400;
+        color: #ffffff;
+      }
+      .sss {
+        top: 168rpx !important;
+      }
+    }
+    .wispNumber {
+      top: 148rpx;
+    }
+
+    .infoBoxFive {
+      top: 192rpx;
+    }
+
+    .infoBoxSix {
+      top: 236rpx;
+    }
+
+    .infoBoxSeven {
+      top: 286rpx;
+    }
+
+    .infoBoxTwo {
+      top: 324rpx;
+    }
+    .infoBoxThree {
+      top: 368rpx;
+      .sss {
+        position: relative;
+        top: 40rpx !important;
+      }
+    }
+    .infoBoxFour {
+      top: 412rpx;
+    }
+
+    .line {
+      width: 684rpx;
+      height: 2rpx;
+      background: linear-gradient(270deg, #182641 0%, #3f547d 49%, #182641 100%);
+      border-radius: 0px 0px 0px 0px;
+      opacity: 1;
+      position: absolute;
+      top: 462rpx;
+    }
+    .btnBox {
+      width: 100%;
+      height: 142rpx;
+      position: absolute;
+      bottom: 0rpx;
+      display: flex;
+      align-items: center;
+      justify-content: space-around;
+      .times {
+        position: absolute;
+        left: 32rpx;
+        width: 119px;
+        height: 22px;
+        font-size: 14px;
+        font-family: PingFang SC-Regular, PingFang SC;
+        font-weight: 400;
+        color: #31e4b9;
+      }
+      .time {
+        position: absolute;
+        right: 32rpx;
+        width: 119px;
+        height: 22px;
+        font-size: 14px;
+        font-family: PingFang SC-Regular, PingFang SC;
+        font-weight: 400;
+        color: #ffffff;
+      }
+      .btn {
+        width: 300rpx;
+        height: 80rpx;
+        background: linear-gradient(270deg, #9331f5 0%, #0b95ff 100%);
+        border-radius: 8px 8px 8px 8px;
+        opacity: 1;
+        font-size: 16px;
+        font-family: PingFang SC-Medium, PingFang SC;
+        font-weight: 500;
+        color: #ffffff;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+      }
+      .boxs {
+        width: 300rpx !important;
+        height: 80rpx !important;
+        padding: 2rpx;
+        border-radius: 8px;
+        border: 1px solid;
+        background-image: linear-gradient(to right bottom, rgba(147, 49, 245, 1), rgba(11, 149, 255, 1));
+        margin-right: 32rpx;
+        .two {
+          width: 300rpx !important;
+          height: 80rpx !important;
+          font-size: 15px;
+          font-family: PingFang SC-Medium, PingFang SC;
+          font-weight: 500;
+          color: #ffffff;
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          background: rgb(25, 38, 68);
+        }
+      }
+    }
+  }
+}
 </style>
--- pages/zhuangrangjilu/zhuangrangjilu.vue
+++ pages/zhuangrangjilu/zhuangrangjilu.vue
@@ -1,128 +1,129 @@
 <template>
-	<view class="history">
-		<view class="item" v-for="(item,index) in list" :key="index">
-			<view class="o"><image :src="item.avatar" mode="widthFix" class="avator"></image></view>
-			<view class="t">
-				<text class="i io" v-if="item.orderType=='PLAYER_TRANSFER_ACCOUNTS'">轉讓GUGU令給予-{{item.targetName}}</text>
-				<!-- <text class="i io" v-if="item.orderType=='SYSTEM_GIVEN'">店小二給予-{{item.targetName}}</text> -->
-				<text class="i io" v-if="item.orderType=='MANAGER_GIVEN'">管理員給予-{{item.targetName}}</text>
-				<text class="i io" v-if="item.orderType=='BOOK_REFUND'">匹配失敗退回gugu令-{{item.targetName}}</text>
-				<text class="i io" v-if="item.orderType=='LAND_REFUND'">匹配失敗退回土地的GUGU令-{{item.targetName}}</text>
-				<text class="i it">{{formatTime(item.createTime)}}</text>
-			</view>
-			<view class="h">
-				<text class="ip">{{item.coin}}</text>
-			</view>
-		</view>
-	</view>
+  <view class="history">
+    <view class="item" v-for="(item, index) in list" :key="index">
+      <view class="o"><image :src="item.avatar" mode="widthFix" class="avator"></image></view>
+      <view class="t">
+        <text class="i io" v-if="item.orderType == 'PLAYER_TRANSFER_ACCOUNTS'">轉讓GUGU令給予-{{ item.targetName }}</text>
+        <!-- <text class="i io" v-if="item.orderType=='SYSTEM_GIVEN'">店小二給予-{{item.targetName}}</text> -->
+        <text class="i io" v-if="item.orderType == 'MANAGER_GIVEN'">管理員給予-{{ item.targetName }}</text>
+        <text class="i io" v-if="item.orderType == 'BOOK_REFUND'">匹配失敗退回gugu令-{{ item.targetName }}</text>
+        <text class="i io" v-if="item.orderType == 'LAND_REFUND'">匹配失敗退回土地的GUGU令-{{ item.targetName }}</text>
+        <!-- <text class="i it">{{formatTime(item.createTime)}}</text> -->
+        <text class="i it">{{ item.createTime }}</text>
+      </view>
+      <view class="h">
+        <text class="ip">{{ item.coin }}</text>
+      </view>
+    </view>
+  </view>
 </template>
 
 <script>
-	export default{
-		data(){
-			return{
-				list:[]
-			}
-		},
-		mounted() {
-			this.load()
-		},
-		methods:{
-			async load(){
-				const res = await this.$api.checkHistory()
-				console.log('------')
-				if(res.code == 200){
-					
-					this.list = res.data.filter(item=>{
-						return item.orderType == 'PLAYER_TRANSFER_ACCOUNTS' || item.orderType == 'MANAGER_GIVEN' 
-						|| item.orderType == 'BOOK_REFUND'|| item.orderType == 'LAND_REFUND'
-					})
-					uni.showToast({
-						title:'獲取數據成功',
-						icon:'success',
-						duration:1000
-					})
-				}else{
-					uni.showToast({
-						title:'獲取數據失敗',
-						icon:'error',
-						duration:1000
-					})
-				}
-				console.log('------',res)
-			},
-			formatTime(time){
-				let date = new Date(time)
-				let year = date.getFullYear()
-				let month = date.getMonth()+1
-				let day = date.getDate()
-				let Hour = date.getHours()
-				 let Min = date.getMinutes()
-				 let Second = date.getSeconds()
-				return `${year}年${month}月${day}日 ${Hour}時${Min}分${Second}秒`
-			}
-		}
-	}
+export default {
+  data () {
+    return {
+      list: []
+    }
+  },
+  mounted () {
+    this.load()
+  },
+  methods: {
+    async load () {
+      const res = await this.$api.checkHistory()
+      console.log('------')
+      if (res.code == 200) {
+
+        this.list = res.data.filter(item => {
+          return item.orderType == 'PLAYER_TRANSFER_ACCOUNTS' || item.orderType == 'MANAGER_GIVEN'
+            || item.orderType == 'BOOK_REFUND' || item.orderType == 'LAND_REFUND'
+        })
+        uni.showToast({
+          title: '獲取數據成功',
+          icon: 'success',
+          duration: 1000
+        })
+      } else {
+        uni.showToast({
+          title: '獲取數據失敗',
+          icon: 'error',
+          duration: 1000
+        })
+      }
+      console.log('------', res)
+    },
+    formatTime (time) {
+      let date = new Date(time)
+      let year = date.getFullYear()
+      let month = date.getMonth() + 1
+      let day = date.getDate()
+      let Hour = date.getHours()
+      let Min = date.getMinutes()
+      let Second = date.getSeconds()
+      return `${year}年${month}月${day}日 ${Hour}時${Min}分${Second}秒`
+    }
+  }
+}
 </script>
 
 <style lang="less">
-	.history{
-		width: 100%;
-		height: 100%;
-		padding-top: 30rpx;
-		background: rgb(5,14,23);
-		.item{
-			width: 90%;
-			height: 140rpx;
-			margin-left: 5%;
-			border-radius: 10rpx;
-			background: rgb(17,24,30);
-			display: flex;
-			margin-bottom: 20rpx;
-			flex-direction: row;
-			.o{
-				width: 25%;
-				height: 100%;
-				display: flex;
-				align-items: center;
-				justify-content: center;
-				.avator{
-					width: 120rpx;
-					height: 120rpx !important;
-					border-radius: 50%;
-				}
-			}
-			.t{
-				width: 50%;
-				height: 100%;
-				display: flex;
-				align-items: flex-start;
-				justify-content: center;
-				flex-direction: column;
-				.i{
-					font-size: 30rpx;
-					color: #FFFFFF;
-				}
-				.io{
-					margin-bottom: 20rpx;
-				}
-				.it{
-					color: grey;
-					font-size: 25rpx;
-				}
-			}
-			.h{
-				width: 25%;
-				height: 100%;
-				display: flex;
-				align-items: center;
-				justify-content: center;
-				.ip{
-					color: #FFFFFF;
-					font-weight: bold;
-					font-size: 45rpx;
-				}
-			}
-		}
-	}
+.history {
+  width: 100%;
+  height: 100%;
+  padding-top: 30rpx;
+  background: rgb(5, 14, 23);
+  .item {
+    width: 90%;
+    height: 140rpx;
+    margin-left: 5%;
+    border-radius: 10rpx;
+    background: rgb(17, 24, 30);
+    display: flex;
+    margin-bottom: 20rpx;
+    flex-direction: row;
+    .o {
+      width: 25%;
+      height: 100%;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      .avator {
+        width: 120rpx;
+        height: 120rpx !important;
+        border-radius: 50%;
+      }
+    }
+    .t {
+      width: 50%;
+      height: 100%;
+      display: flex;
+      align-items: flex-start;
+      justify-content: center;
+      flex-direction: column;
+      .i {
+        font-size: 30rpx;
+        color: #ffffff;
+      }
+      .io {
+        margin-bottom: 20rpx;
+      }
+      .it {
+        color: grey;
+        font-size: 25rpx;
+      }
+    }
+    .h {
+      width: 25%;
+      height: 100%;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      .ip {
+        color: #ffffff;
+        font-weight: bold;
+        font-size: 45rpx;
+      }
+    }
+  }
+}
 </style>
